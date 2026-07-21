4edf0498d5fa87dfda2cfe0a0e913f64b6fde2d0
Nicholas <nhershy@users.noreply.github.com>
2026-07-21 18:00:14 -0500

    Initial commit

---
+++ AppGlide/AppGlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGlideApp.swift
+//  AppGlide
+//
+//  Created by Nicholas Hershy on 7/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AppGlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
