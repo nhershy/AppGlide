79b3ecc1f4bfe5c00fdc8a757630c7a3532965e5
Nicholas <nhershy@users.noreply.github.com>
2026-07-22 18:39:37 -0500

    .

--- AppGlide/AppGlideApp.swift
+++ AppGlide/AppGlideApp.swift
@@ -12,8 +12,12 @@ struct AppGlideApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
 
     var body: some Scene {
-        MenuBarExtra("AppGlide", systemImage: "hand.draw") {
+        MenuBarExtra {
             MenuBarView()
+        } label: {
+            // Template glyph of the app-icon hand and motion streaks
+            // (design/menubar-icon.svg); 16x18 pt.
+            Image("MenuBarIcon")
         }
     }
 }
